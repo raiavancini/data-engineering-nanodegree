9f5fefb7ed92a3384eaa3ea89978afae5375e438
Rai Avancini Franco/KNOX DEV/S2/SIDI <r.franco@samsung.com>
2021-11-23 19:56:41 -0300

    Writing CREATE and DROP statements in sql_queries.py

--- data-modeling-with-postgres/sql_queries.py
+++ data-modeling-with-postgres/sql_queries.py
@@ -1,26 +1,62 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id int PRIMARY KEY,
+    start_time timestamp,
+    user_id int,
+    level text,
+    song_id text,
+    artist_id text,
+    session_id int,
+    location text,
+    user_agent text)
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id int PRIMARY KEY,
+    first_name text,
+    last_name text,
+    gender text,
+    level text)   
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id text PRIMARY KEY,
+    title text,
+    artist_id text,
+    year int,
+    duration numeric)
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id text PRIMARY KEY,
+    name text,
+    location text,
+    latitude numeric,
+    longitude numeric)
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp PRIMARY KEY,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday text)
 """)
 
 # INSERT RECORDS
