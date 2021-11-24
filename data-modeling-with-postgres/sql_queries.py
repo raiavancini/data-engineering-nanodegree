447ce9a04d5fe46976d74784d5d6e72c143e7458
Rai Avancini Franco/KNOX DEV/S2/SIDI <r.franco@samsung.com>
2021-11-23 22:09:13 -0300

    Added INSERT commands for songs and artists table AND followed etl.ipynb until step #3: time Table

--- data-modeling-with-postgres/sql_queries.py
+++ data-modeling-with-postgres/sql_queries.py
@@ -10,52 +10,52 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id int PRIMARY KEY,
+    songplay_id int PRIMARY KEY, 
     start_time timestamp,
-    user_id int,
-    level text,
-    song_id text,
-    artist_id text,
-    session_id int,
-    location text,
+    user_id int, 
+    level text, 
+    song_id text, 
+    artist_id text, 
+    session_id int, 
+    location text, 
     user_agent text)
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id int PRIMARY KEY,
-    first_name text,
-    last_name text,
-    gender text,
+    user_id int PRIMARY KEY, 
+    first_name text, 
+    last_name text, 
+    gender text, 
     level text)   
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
-    song_id text PRIMARY KEY,
-    title text,
-    artist_id text,
-    year int,
+    song_id text PRIMARY KEY, 
+    title text, 
+    artist_id text, 
+    year int, 
     duration numeric)
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
-    artist_id text PRIMARY KEY,
-    name text,
-    location text,
-    latitude numeric,
+    artist_id text PRIMARY KEY, 
+    name text, 
+    location text, 
+    latitude numeric, 
     longitude numeric)
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-    start_time timestamp PRIMARY KEY,
-    hour int,
-    day int,
-    week int,
+    start_time timestamp PRIMARY KEY, 
+    hour int, 
+    day int, 
+    week int, 
     month int,
-    year int,
+    year int, 
     weekday text)
 """)
 
@@ -68,11 +68,16 @@ user_table_insert = ("""
 """)
 
 song_table_insert = ("""
+INSERT INTO songs(song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists(artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING;
 """)
-
 
 time_table_insert = ("""
 """)
