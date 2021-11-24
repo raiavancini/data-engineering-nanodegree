376b42e52fe22e4a6af7abd9fa7488f58917d541
Rai Avancini Franco/KNOX DEV/S2/SIDI <r.franco@samsung.com>
2021-11-24 18:12:28 -0300

    Completed etl.ipynb instructions

--- data-modeling-with-postgres/sql_queries.py
+++ data-modeling-with-postgres/sql_queries.py
@@ -62,9 +62,15 @@ CREATE TABLE IF NOT EXISTS time (
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays(songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (songplay_id) DO NOTHING;
 """)
 
 user_table_insert = ("""
+INSERT INTO users(user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""
@@ -80,11 +86,17 @@ ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 time_table_insert = ("""
+INSERT INTO time(start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+SELECT songs.song_id, artists.artist_id FROM songs 
+JOIN artists ON artists.artist_id = songs.artist_id
+WHERE songs.title = %s and artists.name = %s and songs.duration = %s
 """)
 
 # QUERY LISTS
